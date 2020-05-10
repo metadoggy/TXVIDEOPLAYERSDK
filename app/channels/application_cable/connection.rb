8715b611e8758face0f561ef007df9692786ac2b
metadoggy <105477664+metadoggy@users.noreply.github.com>
2020-05-10 15:45:11 +0000

    scripts/recordmcount.pl: There is no -m32 gcc option on Super-H anymore

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,1 +1,2 @@
-# Be sure to restart your server when you modify this file. Action Cable runs in a loop that does not su
+# Be sure to restart your server when you modify this file. Action Cable runs in a loop that does not support auto reloading.
+module Ap
