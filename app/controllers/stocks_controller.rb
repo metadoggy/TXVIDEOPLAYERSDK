b49a17734451c4f66d83742df304b638320b3927
metadoggy <105477664+metadoggy@users.noreply.github.com>
2020-05-18 15:24:09 +0000

    :art: Update files

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,4 +1,7 @@
 class StocksController < ApplicationController
   before_action :set_stock, only: [:show, :edit, :update, :destroy]
 
-  # GET /stoc
+  # GET /stocks
+  # GET /stocks.json
+  def index
+   
