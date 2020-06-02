c277b043ddf6fb60602e8388e929670c4d2b8c2b
metadoggy <105477664+metadoggy@users.noreply.github.com>
2020-06-02 12:51:50 +0000

    packages updated

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -29,4 +29,6 @@ class StocksController < ApplicationController
     respond_to do |format|
       if @stock.save
         format.html { redirect_to @stock, notice: 'Stock was successfully created.' }
-        format.json { render :show, statu
+        format.json { render :show, status: :created, location: @stock }
+      else
+   
