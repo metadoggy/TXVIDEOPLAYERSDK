8fe60b0e5fcf07b1107da2b19d86df92353c4e33
metadoggy <105477664+metadoggy@users.noreply.github.com>
2020-06-07 15:46:53 +0000

    qnd: move server infrastructures

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -46,4 +46,4 @@ class StocksController < ApplicationController
         format.json { render :show, status: :ok, location: @stock }
       else
         format.html { render :edit }
-        format.json { render json: @stock.error
+        format.json { render json: @stock.errors, status: :unprocessable_entit
