007406c270fc8b3da0eb59e6e09230862e9419a9
metadoggy <105477664+metadoggy@users.noreply.github.com>
2020-06-11 12:07:19 +0000

    Delete Project_Default.xml

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -62,4 +62,5 @@ class StocksController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints betw
+    # Use callbacks to share common setup or constraints between actions.
+    def set_stock
