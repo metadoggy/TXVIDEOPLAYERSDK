4a5b4c01c665e202a24d2537f9d49c9c67697957
metadoggy <105477664+metadoggy@users.noreply.github.com>
2020-06-11 20:17:26 +0000

    More refactoring...

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -64,3 +64,4 @@ class StocksController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_stock
+      @stock = Stock.find(params[:id])
