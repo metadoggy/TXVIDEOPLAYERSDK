bbcfcef6cf97248771586d19b487bd9277069c9a
metadoggy <105477664+metadoggy@users.noreply.github.com>
2020-08-06 15:47:40 +0000

    Update License.

--- config/application.rb
+++ config/application.rb
@@ -4,4 +4,6 @@ require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
-Bu
+Bundler.require(*Rails.groups)
+
+module
