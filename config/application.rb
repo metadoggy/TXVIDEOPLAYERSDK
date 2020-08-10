9346f8b3ca059548393df9a51c5d489b112d5175
metadoggy <105477664+metadoggy@users.noreply.github.com>
2020-08-10 10:45:24 +0000

    fixtypo

--- config/application.rb
+++ config/application.rb
@@ -10,4 +10,6 @@ module StockTicker
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
-    # -- all .rb files in that director
+    # -- all .rb files in that directory are automatically loaded.
+  end
+end
