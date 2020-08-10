093068d01f8fd650b16a4355956c27d31b214996
metadoggy <105477664+metadoggy@users.noreply.github.com>
2020-08-10 09:19:32 +0000

    modify post.html

--- config/application.rb
+++ config/application.rb
@@ -9,4 +9,5 @@ Bundler.require(*Rails.groups)
 module StockTicker
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initialize
+    # Application configuration should go into files in config/initializers
+    # -- all .rb files in that director
