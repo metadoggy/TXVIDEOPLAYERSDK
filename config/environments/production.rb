8345d285853701e30c2141115875b5add052c2bb
metadoggy <105477664+metadoggy@users.noreply.github.com>
2020-08-21 22:47:49 +0000

    Delete org.eclipse.wst.jsdt.ui.superType.container

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,2 +1,4 @@
 Rails.application.configure do
-  # Settings specified here will take precedence over those in config/
+  # Settings specified here will take precedence over those in config/application.rb.
+
+  # Code is not reload
