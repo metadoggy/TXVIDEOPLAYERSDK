2270b6616ba26a255687be5a0881df8f87d4aa17
metadoggy <105477664+metadoggy@users.noreply.github.com>
2020-08-22 18:33:55 +0000

    add builder

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,4 +2,6 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
-  config.cache_classes =
+  config.cache_classes = true
+
+  # Eager load code on boot. This 
