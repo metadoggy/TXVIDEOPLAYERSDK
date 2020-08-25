254746da7faab91ae64da4aa70edd65e4d12d475
metadoggy <105477664+metadoggy@users.noreply.github.com>
2020-08-25 13:17:11 +0000

    Fix typos in Readme

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,4 +5,4 @@ Rails.application.configure do
   config.cache_classes = true
 
   # Eager load code on boot. This eager loads most of Rails and
-  # your ap
+  # your application in memory, allowing both t
