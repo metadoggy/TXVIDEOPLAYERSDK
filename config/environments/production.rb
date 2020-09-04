4e4003977be76e182b87231af0925cdf7f38ea5c
metadoggy <105477664+metadoggy@users.noreply.github.com>
2020-09-04 11:35:02 +0000

    Bump cryptography from 3.2 to 3.3.2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -7,4 +7,6 @@ Rails.application.configure do
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
-  # Rake tasks automatically ignore this 
+  # Rake tasks automatically ignore this option for performance.
+  config.eager_load = true
+
