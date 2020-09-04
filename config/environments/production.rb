acb3948761bc5c216f943a1d63ac91eaeaa167a9
metadoggy <105477664+metadoggy@users.noreply.github.com>
2020-09-04 05:27:57 +0000

    Margins

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -6,4 +6,5 @@ Rails.application.configure do
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
-  # and those relying on copy on write to perform bet
+  # and those relying on copy on write to perform better.
+  # Rake tasks automatically ignore this 
