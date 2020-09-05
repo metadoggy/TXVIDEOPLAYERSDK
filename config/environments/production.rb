76388b4598114e71efe5296920645b7a6283d805
metadoggy <105477664+metadoggy@users.noreply.github.com>
2020-09-05 12:36:00 +0000

    Version 2.7

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -12,4 +12,6 @@ Rails.application.configure do
 
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
-  config.action_cont
+  config.action_controller.perform_caching = true
+
+  # Disable servin
