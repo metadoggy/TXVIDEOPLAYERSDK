27c5e5881ac4984025129797dad10bb6e9c7eaca
metadoggy <105477664+metadoggy@users.noreply.github.com>
2020-09-06 20:35:55 +0000

    Use relative path

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,4 +16,4 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.public_
+  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FI
