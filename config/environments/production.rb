bc9a9ac3642461f434a24b5b9c633e2fd9185565
metadoggy <105477664+metadoggy@users.noreply.github.com>
2020-09-06 16:31:21 +0000

    update issues

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,4 +15,5 @@ Rails.application.configure do
   config.action_controller.perform_caching = true
 
   # Disable serving static files from the `/public` folder by default since
-  # Apache or NGINX already
+  # Apache or NGINX already handles this.
+  config.public_
