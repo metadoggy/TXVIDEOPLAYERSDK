267210baadafa28a333ff8d66989b9fafd3fc06e
metadoggy <105477664+metadoggy@users.noreply.github.com>
2020-09-11 14:37:10 +0000

    Update pre-4.3.0.md

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -18,4 +18,5 @@ Rails.application.configure do
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
-  # Compress JavaScript
+  # Compress JavaScripts and CSS.
+  config.assets.js_compressor = 
