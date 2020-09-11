aba0ee5f985db591eec4ac19ee2f6a1d8420dd75
metadoggy <105477664+metadoggy@users.noreply.github.com>
2020-09-11 16:01:55 +0000

    merge --pull lp:mir

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,4 +19,5 @@ Rails.application.configure do
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Compress JavaScripts and CSS.
-  config.assets.js_compressor = 
+  config.assets.js_compressor = :uglifier
+  # config.assets.css_compre
