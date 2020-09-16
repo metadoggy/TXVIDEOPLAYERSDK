d9a2854d9f41e165fd9eb62c57bd46703308218c
metadoggy <105477664+metadoggy@users.noreply.github.com>
2020-09-16 13:28:28 +0000

    docs(changelog)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,4 +27,6 @@ Rails.application.configure do
 
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
-  config.ass
+  config.assets.digest = true
+
+  # `config.assets.pr
