b0715f330ccf27aa4568adf24a5b233c5fb04ed9
metadoggy <105477664+metadoggy@users.noreply.github.com>
2020-09-17 22:14:27 +0000

    Release 1.0.1

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,4 +29,6 @@ Rails.application.configure do
   # yet still be able to expire them through the digest params.
   config.assets.digest = true
 
-  # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/asset
+  # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
+
+  # Enable serving of images, sty
