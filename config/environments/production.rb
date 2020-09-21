13e913a7f7030ac827ca64f93bc0f96df78b1e5b
metadoggy <105477664+metadoggy@users.noreply.github.com>
2020-09-21 18:12:05 +0000

    Clean up dependencies

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -32,4 +32,6 @@ Rails.application.configure do
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  # config.action_controller.asset_h
+  # config.action_controller.asset_host = 'http://assets.example.com'
+
+  #
