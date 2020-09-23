ffd13a254eb8810c96a25abf5f5d9b4538d06228
metadoggy <105477664+metadoggy@users.noreply.github.com>
2020-09-23 12:27:06 +0000

    fix windows build

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -34,4 +34,4 @@ Rails.application.configure do
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
 
-  #
+  # Specifies the header that your server uses 
