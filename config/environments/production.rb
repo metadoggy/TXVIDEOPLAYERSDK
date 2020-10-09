f3a6a2e64118441e530f4185aeb5050ad404f6c8
metadoggy <105477664+metadoggy@users.noreply.github.com>
2020-10-09 13:13:58 +0000

    removed redundant imports

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -39,4 +39,5 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
 
   # Action Cable endpoint configuration
-  # config.action_cable.url = 'wss://example.
+  # config.action_cable.url = 'wss://example.com/cable'
+  # config.action_cable.a
