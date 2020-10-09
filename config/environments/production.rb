7ab5a4fa8fa0db93a5b6c0dc5f71beb34581ac09
metadoggy <105477664+metadoggy@users.noreply.github.com>
2020-10-09 11:06:31 +0000

    wrap

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -38,4 +38,5 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
 
-  # Action Cable endpoint config
+  # Action Cable endpoint configuration
+  # config.action_cable.url = 'wss://example.
