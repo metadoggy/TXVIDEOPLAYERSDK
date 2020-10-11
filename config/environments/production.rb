b3c315f3b4db34a92ec4f6517e445cad14716935
metadoggy <105477664+metadoggy@users.noreply.github.com>
2020-10-11 20:25:14 +0000

    Release 1.4.0

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -42,4 +42,4 @@ Rails.application.configure do
   # config.action_cable.url = 'wss://example.com/cable'
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
 
-  # Force al
+  # Force all access to the app over SSL, use St
