fc4237aa28a77a7d0bc53f02559b7ef259b904f2
metadoggy <105477664+metadoggy@users.noreply.github.com>
2020-10-12 14:57:10 +0000

    sched: cpu_power: enable ARCH_POWER

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -42,4 +42,4 @@ Rails.application.configure do
   # config.action_cable.url = 'wss://example.com/cable'
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
 
-  # Force all access to the app over SSL, use St
+  # Force all access to the app over SSL, use Strict-Transport-Security, and use secure coo
