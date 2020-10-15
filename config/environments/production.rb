1b552080005cb7cabdc3c3a5fc1eb49405bffeb8
metadoggy <105477664+metadoggy@users.noreply.github.com>
2020-10-15 13:33:58 +0000

    Clean-up

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -45,4 +45,5 @@ Rails.application.configure do
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
 
-  # Use the lowest log level to ensure availability
+  # Use the lowest log level to ensure availability of diagnostic information
+  # 
