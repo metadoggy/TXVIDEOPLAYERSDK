877051484919b1c0db12d93c28ff671a36ed3f91
metadoggy <105477664+metadoggy@users.noreply.github.com>
2020-10-16 13:03:03 +0000

    ignoring some files

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -49,4 +49,5 @@ Rails.application.configure do
   # when problems arise.
   config.log_level = :debug
 
-  # Prepend all log lines with the followi
+  # Prepend all log lines with the following tags.
+  config.log_tags = [ :request
