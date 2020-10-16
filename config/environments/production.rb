8021739ef0c46b33f2f7171454638dbb15b92c92
metadoggy <105477664+metadoggy@users.noreply.github.com>
2020-10-16 09:30:52 +0000

    mmc: Add status IRQ and status callback function to mmc platform data

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,4 +47,6 @@ Rails.application.configure do
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
-  config.log_level =
+  config.log_level = :debug
+
+  # Prepend all log lines with the followi
