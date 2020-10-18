6a9c4f95801651ce76c4c14071e36c85c06cf683
metadoggy <105477664+metadoggy@users.noreply.github.com>
2020-10-18 20:39:40 +0000

    small text changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -54,4 +54,6 @@ Rails.application.configure do
 
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
-  # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-na
+  # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
+
+  if ENV["RAILS_LOG_TO_STDOUT"]
