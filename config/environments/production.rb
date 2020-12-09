1b463112dd85d1d4ce11d63281fa7fb1a2d67725
metadoggy <105477664+metadoggy@users.noreply.github.com>
2020-12-09 12:46:03 +0000

    Update package info.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,3 +64,5 @@ Rails.application.configure do
   # config.cache_store = :mem_cache_store
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
+  # config.active_job.queue_adapter     = :resque
+  # conf
