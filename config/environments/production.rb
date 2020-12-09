7494a5c91d17b93668c395df6a25d18c60de074a
metadoggy <105477664+metadoggy@users.noreply.github.com>
2020-12-09 17:25:59 +0000

    Publishing

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,4 +65,5 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "stock-ticke
+  # config.active_job.queue_name_prefix = "stock-ticker_#{Rails.env}"
+  config.action_mailer.perform_
