ca5d06e28e0e526b5366f543eba56e79712fbb76
metadoggy <105477664+metadoggy@users.noreply.github.com>
2020-12-24 07:56:22 +0000

    INSPECTIT-2199 Create inspectIT marketplace prototype

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,4 +69,5 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
-  # Set this to true and configure the email server for immediate delivery to raise delivery err
+  # Set this to true and configure the email server for immediate delivery to raise delivery errors.
+  # config.action_mailer.raise_de
