93e2f7f76cde4e1ceedc5e35f68b41464c3573b5
metadoggy <105477664+metadoggy@users.noreply.github.com>
2021-01-02 21:20:17 +0000

    Translation updates

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,5 @@ Rails.application.configure do
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
 
-  # Send deprecation notices to
+  # Send deprecation notices to registered listeners.
+  config.activ
