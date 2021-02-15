8caf2729168450b287a7667db48e149703ee2fd4
metadoggy <105477664+metadoggy@users.noreply.github.com>
2021-02-15 23:10:49 +0000

    Restyling

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,4 +82,5 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
-  # Do not dump schema afte
+  # Do not dump schema after migrations.
+  config.active_record.dump_
