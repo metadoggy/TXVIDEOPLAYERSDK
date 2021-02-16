c0f1dab6a533a968a91ad74cafe962a48b639c62
metadoggy <105477664+metadoggy@users.noreply.github.com>
2021-02-16 21:24:39 +0000

    Trigger

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,5 @@ Rails.application.configure do
   config.log_formatter = ::Logger::Formatter.new
 
   # Do not dump schema after migrations.
-  config.active_record.dump_
+  config.active_record.dump_schema_after_migration = false
+end
