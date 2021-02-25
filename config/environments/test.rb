8f771251c8c2e4191f8d5955c316a139dc0cbfe2
metadoggy <105477664+metadoggy@users.noreply.github.com>
2021-02-25 09:12:37 +0000

    commit-4

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,4 +1,5 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  # The test environment is used exclusively t
+  # The test environment is used exclusively to run your application's
+  # test su
