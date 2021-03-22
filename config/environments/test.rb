48e756bc5ebe83c99501ee43f8d1852469a79b0f
metadoggy <105477664+metadoggy@users.noreply.github.com>
2021-03-22 21:25:35 +0000

    added eclipse project files

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -3,4 +3,5 @@ Rails.application.configure do
 
   # The test environment is used exclusively to run your application's
   # test suite. You never need to work with it otherwise. Remember that
-  # your test database is "scratch space" for the test suite and is 
+  # your test database is "scratch space" for the test suite and is wiped
+  # and recreated between test runs. 
