1e3671fbc9fd53006c5d16a83ba983245a2ab8e9
metadoggy <105477664+metadoggy@users.noreply.github.com>
2021-03-22 16:21:35 +0000

    fix .travis.yml

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -3,4 +3,4 @@ Rails.application.configure do
 
   # The test environment is used exclusively to run your application's
   # test suite. You never need to work with it otherwise. Remember that
-  # your test database is 
+  # your test database is "scratch space" for the test suite and is 
