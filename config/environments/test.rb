4f54df1cf83c1e3a7b91459b2851d2753ef51b4a
metadoggy <105477664+metadoggy@users.noreply.github.com>
2021-03-30 15:23:30 +0000

    Added API diff to CHANGELOG.md.

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -4,4 +4,5 @@ Rails.application.configure do
   # The test environment is used exclusively to run your application's
   # test suite. You never need to work with it otherwise. Remember that
   # your test database is "scratch space" for the test suite and is wiped
-  # and recreated between test runs. 
+  # and recreated between test runs. Don't rely on the data there!
+  config.cache
