30a93111a8de3affd89fc29971ed195187369634
metadoggy <105477664+metadoggy@users.noreply.github.com>
2021-04-15 06:33:15 +0000

    Minor code refactoring.

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -9,4 +9,5 @@ Rails.application.configure do
 
   # Do not eager load code on boot. This avoids loading your whole application
   # just for the purpose of running a single test. If you are using a tool that
-  # preloads Rails for running tests, you may hav
+  # preloads Rails for running tests, you may have to set it to true.
+  confi
