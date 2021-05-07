86568ec04a66ef2be8616ed4734eee2ca20bd299
metadoggy <105477664+metadoggy@users.noreply.github.com>
2021-05-07 12:28:47 +0000

    Update to EIK parent 0.9.0

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -12,4 +12,4 @@ Rails.application.configure do
   # preloads Rails for running tests, you may have to set it to true.
   config.eager_load = false
 
-  # Configu
+  # Configure public file server for tests 
