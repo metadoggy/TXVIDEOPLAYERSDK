9bb5a83ad3e093d160011ab7917c9ede23b44aa7
metadoggy <105477664+metadoggy@users.noreply.github.com>
2021-06-02 22:56:38 +0000

    V1

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -15,4 +15,7 @@ Rails.application.configure do
   # Configure public file server for tests with Cache-Control for performance.
   config.public_file_server.enabled = true
   config.public_file_server.headers = {
-    'Cache
+    'Cache-Control' => 'public, max-age=3600'
+  }
+
+  # Sh
