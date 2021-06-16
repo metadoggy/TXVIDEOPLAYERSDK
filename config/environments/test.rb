20749477b5de0ce511c304b4022a149a1e1bb787
metadoggy <105477664+metadoggy@users.noreply.github.com>
2021-06-16 19:01:46 +0000

    Removed some debug code

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -22,4 +22,5 @@ Rails.application.configure do
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
-  # Raise exceptions instead of rendering
+  # Raise exceptions instead of rendering exception templates.
+  config.action_
