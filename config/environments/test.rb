2c44deb65563d1398b228f5182f64d07d71afc99
metadoggy <105477664+metadoggy@users.noreply.github.com>
2021-06-16 16:45:03 +0000

    staging: zsmalloc: Rename page_mapcount_reset

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -20,4 +20,6 @@ Rails.application.configure do
 
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
-  config.action_controller.perform_caching = 
+  config.action_controller.perform_caching = false
+
+  # Raise exceptions instead of rendering
