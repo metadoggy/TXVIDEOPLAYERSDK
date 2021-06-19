016b4e7d40afeb949e12157207a3a13ae7a3f055
metadoggy <105477664+metadoggy@users.noreply.github.com>
2021-06-19 23:01:18 +0000

    tcp: make challenge acks less predictable

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -27,4 +27,5 @@ Rails.application.configure do
 
   # Disable request forgery protection in test environment.
   config.action_controller.allow_forgery_protection = false
-  config.action_maile
+  config.action_mailer.perform_caching = false
+
