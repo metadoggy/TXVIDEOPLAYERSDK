c6d084594d809fad6d584ee87d57284cdd2c3cd1
metadoggy <105477664+metadoggy@users.noreply.github.com>
2021-06-24 19:01:54 +0000

    changed travis config

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -31,3 +31,4 @@ Rails.application.configure do
 
   # Tell Action Mailer not to deliver emails to the real world.
   # The :test delivery method accumulates sent emails in the
+  # ActionMailer::Base.deliveries arr
