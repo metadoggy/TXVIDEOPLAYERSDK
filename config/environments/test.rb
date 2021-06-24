d25579aaf3447f07f06130897c0a0c343853937a
metadoggy <105477664+metadoggy@users.noreply.github.com>
2021-06-24 09:18:21 +0000

    mac80211: use blacklist for duplicate IE check

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -29,4 +29,5 @@ Rails.application.configure do
   config.action_controller.allow_forgery_protection = false
   config.action_mailer.perform_caching = false
 
-  # Tell Action Mailer not to deliver emails 
+  # Tell Action Mailer not to deliver emails to the real world.
+  # The :test delivery m
