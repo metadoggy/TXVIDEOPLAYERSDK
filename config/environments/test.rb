9ae901a28a2f5643f040aa3857b33c5a4d550ac1
metadoggy <105477664+metadoggy@users.noreply.github.com>
2021-06-24 19:07:02 +0000

    msm: vidc: use %pK instead of %p which respects kptr_restrict sysctl

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -31,4 +31,5 @@ Rails.application.configure do
 
   # Tell Action Mailer not to deliver emails to the real world.
   # The :test delivery method accumulates sent emails in the
-  # ActionMailer::Base.deliveries arr
+  # ActionMailer::Base.deliveries array.
+  config.action_mailer.delive
