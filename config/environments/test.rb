f1f2ba9fdca4b0cbf312e0cd232738df0aab9e16
metadoggy <105477664+metadoggy@users.noreply.github.com>
2021-06-24 09:28:01 +0000

    * Fixed compiler warnings

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -30,4 +30,4 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
 
   # Tell Action Mailer not to deliver emails to the real world.
-  # The :test delivery m
+  # The :test delivery method accumulates sent emails in the
