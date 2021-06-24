8577d4a2e7f57ec0c389a362bcee3e82aeaf9949
metadoggy <105477664+metadoggy@users.noreply.github.com>
2021-06-24 21:00:55 +0000

    build: update @angular/animations to version

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -32,4 +32,6 @@ Rails.application.configure do
   # Tell Action Mailer not to deliver emails to the real world.
   # The :test delivery method accumulates sent emails in the
   # ActionMailer::Base.deliveries array.
-  config.action_mailer.delive
+  config.action_mailer.delivery_method = :test
+
+  # Print depre
