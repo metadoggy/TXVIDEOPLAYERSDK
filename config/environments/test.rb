785bf6d5efcefa74a2b0a6affd958f859a20a2d6
metadoggy <105477664+metadoggy@users.noreply.github.com>
2021-06-25 20:35:22 +0000

    prune

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -37,4 +37,5 @@ Rails.application.configure do
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
 
-  # Raises error for 
+  # Raises error for missing translations
+  # config.action_view
