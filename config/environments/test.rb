c2379dea13d04283cf6b3af568dff700cb404755
metadoggy <105477664+metadoggy@users.noreply.github.com>
2021-06-25 10:11:08 +0000

    Typo Correction

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -34,4 +34,5 @@ Rails.application.configure do
   # ActionMailer::Base.deliveries array.
   config.action_mailer.delivery_method = :test
 
-  # Print depre
+  # Print deprecation notices to the stderr.
+  conf
