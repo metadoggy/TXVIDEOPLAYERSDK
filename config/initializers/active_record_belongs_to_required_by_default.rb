0c26c6e15eafe54fce6bf0afacc8cec4a216f985
metadoggy <105477664+metadoggy@users.noreply.github.com>
2021-07-12 15:07:54 +0000

    Use latest makexpi

--- config/initializers/active_record_belongs_to_required_by_default.rb
+++ config/initializers/active_record_belongs_to_required_by_default.rb
@@ -1,4 +1,4 @@
 # Be sure to restart your server when you modify this file.
 
 # Require `belongs_to` associations by default. This is a new Rails 5.0
-# default, so it is introd
+# default, so it is introduced as a configuration option to ensure
