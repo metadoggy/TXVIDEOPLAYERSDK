69c7985999dd099162d791c7b6ec807bdad8149b
metadoggy <105477664+metadoggy@users.noreply.github.com>
2021-07-12 08:25:57 +0000

    Prepare 1.1.5 release

--- config/initializers/active_record_belongs_to_required_by_default.rb
+++ config/initializers/active_record_belongs_to_required_by_default.rb
@@ -1,3 +1,4 @@
 # Be sure to restart your server when you modify this file.
 
-# Require `belongs_to` associations by default. This is a new Rai
+# Require `belongs_to` associations by default. This is a new Rails 5.0
+# default, so it is introd
