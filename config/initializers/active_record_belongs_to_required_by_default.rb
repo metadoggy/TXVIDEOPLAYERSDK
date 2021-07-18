801abbcfe27d8a0f85e0a45a7a5f0f66179c1dc2
metadoggy <105477664+metadoggy@users.noreply.github.com>
2021-07-18 21:20:30 +0000

    added more unit tests

--- config/initializers/active_record_belongs_to_required_by_default.rb
+++ config/initializers/active_record_belongs_to_required_by_default.rb
@@ -1,4 +1,5 @@
 # Be sure to restart your server when you modify this file.
 
 # Require `belongs_to` associations by default. This is a new Rails 5.0
-# default, so it is introduced as a configuration option to ensure
+# default, so it is introduced as a configuration option to ensure that apps
+# made on earlier versions of Rails are not af
