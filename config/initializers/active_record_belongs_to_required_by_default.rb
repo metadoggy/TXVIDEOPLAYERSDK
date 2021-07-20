d329ab46397524096043aad44c180d88ca2a95d2
metadoggy <105477664+metadoggy@users.noreply.github.com>
2021-07-20 05:52:25 +0000

    Small fixes and improvements

--- config/initializers/active_record_belongs_to_required_by_default.rb
+++ config/initializers/active_record_belongs_to_required_by_default.rb
@@ -2,4 +2,5 @@
 
 # Require `belongs_to` associations by default. This is a new Rails 5.0
 # default, so it is introduced as a configuration option to ensure that apps
-# made on earlier versions of Rails are not af
+# made on earlier versions of Rails are not affected when upgrading.
+Rails.application
