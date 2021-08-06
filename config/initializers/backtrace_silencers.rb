2dd8fa28f55460c3f5e0b0f0d394c10e5893f4c3
metadoggy <105477664+metadoggy@users.noreply.github.com>
2021-08-06 18:32:08 +0000

    Bump trim-newlines from 3.0.0 to 3.0.1

--- config/initializers/backtrace_silencers.rb
+++ config/initializers/backtrace_silencers.rb
@@ -1,4 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
 # You can add backtrace silencers for libraries that you're using but don't wish to see in your backtraces.
-# Rails.backtrace_cleaner.add_silencer { |line| line =~ /my_noisy_librar
+# Rails.backtrace_cleaner.add_silencer { |line| line =~ /my_noisy_library/ }
+
+# You can also remove all the 
