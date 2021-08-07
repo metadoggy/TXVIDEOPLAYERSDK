49e03f22d8aa9dd8120b96130163d34b70d47fc0
metadoggy <105477664+metadoggy@users.noreply.github.com>
2021-08-07 21:52:54 +0000

    Bumped version to 3.0.0

--- config/initializers/backtrace_silencers.rb
+++ config/initializers/backtrace_silencers.rb
@@ -3,4 +3,4 @@
 # You can add backtrace silencers for libraries that you're using but don't wish to see in your backtraces.
 # Rails.backtrace_cleaner.add_silencer { |line| line =~ /my_noisy_library/ }
 
-# You can also remove all the 
+# You can also remove all the silencers if you're trying to debug a probl
