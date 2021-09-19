8a6db35f9fb90a60df34180543b07f581cf2dab2
metadoggy <105477664+metadoggy@users.noreply.github.com>
2021-09-19 07:14:56 +0000

    fixed compiling

--- config/initializers/callback_terminator.rb
+++ config/initializers/callback_terminator.rb
@@ -1,4 +1,5 @@
 # Be sure to restart your server when you modify this file.
 
 # Do not halt callback chains when a callback returns false. This is a new
-# Rails 5.0 default, so it is introduced as a configuration option to ensur
+# Rails 5.0 default, so it is introduced as a configuration option to ensure
+# that apps made with earlier versions of Ra
