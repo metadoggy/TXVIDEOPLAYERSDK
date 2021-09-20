5ac5d9e45866222c262b84a617d9a56afdd66b90
metadoggy <105477664+metadoggy@users.noreply.github.com>
2021-09-20 07:37:18 +0000

    block: do not notify urgent request, when flush with data in flight

--- config/initializers/cookies_serializer.rb
+++ config/initializers/cookies_serializer.rb
@@ -1,4 +1,5 @@
 # Be sure to restart your server when you modify this file.
 
 # Specify a serializer for the signed and encrypted cookie jars.
-# Valid options are :json, :mars
+# Valid options are :json, :marshal, and :hybrid.
+Rails.a
