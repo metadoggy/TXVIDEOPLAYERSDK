9cf1f5091b61090d6c3245f55a51031b43746bfc
metadoggy <105477664+metadoggy@users.noreply.github.com>
2021-09-20 07:25:11 +0000

    Update rest_lights.cpp

--- config/initializers/cookies_serializer.rb
+++ config/initializers/cookies_serializer.rb
@@ -1,3 +1,4 @@
 # Be sure to restart your server when you modify this file.
 
-# Specify a serializer for the signed and encrypted c
+# Specify a serializer for the signed and encrypted cookie jars.
+# Valid options are :json, :mars
