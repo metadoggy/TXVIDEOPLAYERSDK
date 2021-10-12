97d1aef7ef7eaa120ebb6322076d0eecccd3979f
metadoggy <105477664+metadoggy@users.noreply.github.com>
2021-10-12 18:06:07 +0000

    ToC

--- config/initializers/inflections.rb
+++ config/initializers/inflections.rb
@@ -2,4 +2,5 @@
 
 # Add new inflection rules using the following format. Inflections
 # are locale specific, and you may define rules for as many different
-# locales as you wish. All of these examples are
+# locales as you wish. All of these examples are active by default:
+# ActiveSupport::
