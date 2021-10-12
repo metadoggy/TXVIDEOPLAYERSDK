fef188a0669c514ff330baac2b860b032943fa3b
metadoggy <105477664+metadoggy@users.noreply.github.com>
2021-10-12 17:17:42 +0000

    update control

--- config/initializers/inflections.rb
+++ config/initializers/inflections.rb
@@ -1,4 +1,5 @@
 # Be sure to restart your server when you modify this file.
 
 # Add new inflection rules using the following format. Inflections
-# are locale specific, and you may defi
+# are locale specific, and you may define rules for as many different
+# locales as yo
