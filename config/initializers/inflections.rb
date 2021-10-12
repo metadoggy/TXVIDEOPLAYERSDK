b36d456f51589427cbfbc5caaeb94e8355b1b0bd
metadoggy <105477664+metadoggy@users.noreply.github.com>
2021-10-12 20:43:31 +0000

    changed titles

--- config/initializers/inflections.rb
+++ config/initializers/inflections.rb
@@ -3,4 +3,5 @@
 # Add new inflection rules using the following format. Inflections
 # are locale specific, and you may define rules for as many different
 # locales as you wish. All of these examples are active by default:
-# ActiveSupport::
+# ActiveSupport::Inflector.inflections(:en) do |inflect|
+#   
