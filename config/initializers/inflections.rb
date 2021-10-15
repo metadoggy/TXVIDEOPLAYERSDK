7b0890eabc1d8e1200f8a11f7019fbc3f7f3eae6
metadoggy <105477664+metadoggy@users.noreply.github.com>
2021-10-15 19:02:55 +0000

    removed debug logs

--- config/initializers/inflections.rb
+++ config/initializers/inflections.rb
@@ -4,4 +4,5 @@
 # are locale specific, and you may define rules for as many different
 # locales as you wish. All of these examples are active by default:
 # ActiveSupport::Inflector.inflections(:en) do |inflect|
-#   
+#   inflect.plural /^(ox)$/i, '\1en'
+#   inf
