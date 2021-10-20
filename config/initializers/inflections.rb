804b818554f72dde52d8746c162a9103f9f3c9ea
metadoggy <105477664+metadoggy@users.noreply.github.com>
2021-10-20 12:56:31 +0000

    updating french translations

--- config/initializers/inflections.rb
+++ config/initializers/inflections.rb
@@ -7,4 +7,7 @@
 #   inflect.plural /^(ox)$/i, '\1en'
 #   inflect.singular /^(ox)en/i, '\1'
 #   inflect.irregular 'person', 'people'
-#   inflect.uncountabl
+#   inflect.uncountable %w( fish sheep )
+# end
+
+# These inflectio
