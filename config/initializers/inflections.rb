eab0431ddf1c13854ce2e0df89639f72c1a578f3
metadoggy <105477664+metadoggy@users.noreply.github.com>
2021-10-20 08:04:20 +0000

    version bump to 0.1.4

--- config/initializers/inflections.rb
+++ config/initializers/inflections.rb
@@ -6,4 +6,5 @@
 # ActiveSupport::Inflector.inflections(:en) do |inflect|
 #   inflect.plural /^(ox)$/i, '\1en'
 #   inflect.singular /^(ox)en/i, '\1'
-#   inflect
+#   inflect.irregular 'person', 'people'
+#   inflect.uncountabl
