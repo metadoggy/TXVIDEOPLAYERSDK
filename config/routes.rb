b5615532de27c63f6f709f50934477abce49bbd6
metadoggy <105477664+metadoggy@users.noreply.github.com>
2022-01-29 09:33:52 +0000

    Added Little Endian support to vtpm module

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   resources :stocks
-  # For details on the DSL available within this file, see http://guide
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # Serve we
