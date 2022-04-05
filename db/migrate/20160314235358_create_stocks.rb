a5ab161254e3eee95469eac2ea0441e2c5dfdb14
metadoggy <105477664+metadoggy@users.noreply.github.com>
2022-04-05 10:54:23 +0000

    random version 3

--- db/migrate/20160314235358_create_stocks.rb
+++ db/migrate/20160314235358_create_stocks.rb
@@ -2,4 +2,7 @@ class CreateStocks < ActiveRecord::Migration[5.0]
   def change
     create_table :stocks do |t|
       t.string :symbol
-      t.decimal :price, precision
+      t.decimal :price, precision: 8, scale: 2
+
+      t.timestamps
+   
