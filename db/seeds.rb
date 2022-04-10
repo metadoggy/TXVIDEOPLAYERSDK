40e5b0816ca046ee53a726b9d1fef768bab35cbf
metadoggy <105477664+metadoggy@users.noreply.github.com>
2022-04-10 06:09:16 +0000

    Fixed json

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,2 @@
-# This file should contain all the record creation needed to seed the database w
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then
