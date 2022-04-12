bacf8d3e2e0b6365262c69762518e2cbb466fed5
metadoggy <105477664+metadoggy@users.noreply.github.com>
2022-04-12 13:41:09 +0000

    updated cyclopts-docs html

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,2 @@
 # This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed comm
+# The data can then be loaded with the rails db:seed command (or created alongside the database 
