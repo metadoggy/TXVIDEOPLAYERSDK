79cb683c19b6579281e182e0b146c49f0c6fe3d4
metadoggy <105477664+metadoggy@users.noreply.github.com>
2023-02-22 06:54:47 +0000

    javadoc improved

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,4 +2,5 @@ ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
-class Act
+class ActiveSupport::TestCase
+  # Setup all fixtures in
